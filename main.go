081112ffa0f1a78ab1df03bdfa4208442f29fdd3
unknown <>
2017-04-24 09:40:42 +0800

    增加定时检查功能

--- main.go
+++ main.go
@@ -7,24 +7,36 @@ import (
 	"net/http"
 	"strings"
 	"github.com/go-gomail/gomail"
+	"os"
+	"time"
+	"flag"
+	"log"
 )
-
+var oldIp = ""
 func main() {
-	resp, _ := http.Get("http://pv.sohu.com/cityjson?ie=utf-8")
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	js := string(body)
-	ip := strings.Split(js, `=`)[1]
-	ipJson := strings.TrimSpace(ip)
-
-	if strings.HasSuffix(ipJson, `;`) {
-		ipJson = ipJson[0 : len(ipJson)-1]
+	interval := flag.Int64("i",30,"检查间隔,单位:秒")
+	flag.Parse()
+	log.Println("已经设置检查间隔:",*interval)
+	defer func(){ // 必须要先声明defer，否则不能捕获到panic异常
+		if err:=recover();err!=nil{
+			log.Println(err) // 这里的err其实就是panic传入的内容
+		}
+	}()
+	ticker := time.NewTicker(time.Second * time.Duration(*interval))
+	for _ = range ticker.C{
+		ip := getPublicIp()
+		if len(ip) > 0 && oldIp != ip{
+			sendEmail(ip)
+		}
+		oldIp = ip
 	}
-
-	IP,_ := simplejson.NewJson([]byte(ipJson))
-	ipAddress,_ := IP.Get("cip").String()
-	fmt.Println(ipAddress)
-	subject := "家里的IP地址已变更为:"
+}
+func sendEmail(ip string){
+	hostName,err := os.Hostname()
+	if err != nil {
+		log.Println(err)
+	}
+	subject := "[" + time.Now().Format("2006-01-02 15:04:05") + "]" + hostName + "IP地址已变更为:"
 	content := fmt.Sprintf(`
 		<html>
 		<body>
@@ -33,7 +45,7 @@ func main() {
 		</h3>
 		</body>
 		</html>
-		`,ipAddress)
+		`,ip)
 	fmt.Println("send email")
 	msg := gomail.NewMessage()
 	msg.SetHeader("From", "454679718@163.com")
@@ -45,6 +57,34 @@ func main() {
 	// Send the email to Bob, Cora and Dan
 	mailer := gomail.NewDialer("smtp.163.com",25, "454679718@163.com", "brady1988")
 	if err := mailer.DialAndSend(msg); err != nil {
-		panic(err)
+		log.Println(err)
 	}
+}
+func getPublicIp() string{
+	resp, err := http.Get("http://pv.sohu.com/cityjson?ie=utf-8")
+	if err != nil {
+		return ""
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return ""
+	}
+	js := string(body)
+	ip := strings.Split(js, `=`)[1]
+	ipJson := strings.TrimSpace(ip)
+
+	if strings.HasSuffix(ipJson, `;`) {
+		ipJson = ipJson[0 : len(ipJson)-1]
+	}
+
+	IP,err := simplejson.NewJson([]byte(ipJson))
+	if err != nil {
+		return ""
+	}
+	ipAddress,err := IP.Get("cip").String()
+	if err != nil {
+		return ""
+	}
+	return ipAddress
 }
